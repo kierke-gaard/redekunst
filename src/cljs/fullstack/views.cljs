ff1c89a98199270a9c9a0f8b2e6fc06e2ed08c8b
Jan Hendrik Clausen <jan-hendrik.clausen@data42.de>
2019-01-11 14:08:10 +0100

    added some content to windows. Button

--- src/cljs/fullstack/views.cljs
+++ src/cljs/fullstack/views.cljs
@@ -7,7 +7,7 @@
 ;; FRAME ====================================
 ;; :suche.core/search-open
 ;; woco.frame/create-frame
-(defn panel [{:keys [:id :x :y :w :h :title]}]
+(defn panel [{:keys [:id :x :y :w :h :title :content]}]
   [:div.optimist__window {:id id
                           :style {:position "absolute"
                                   :left x
@@ -16,13 +16,45 @@
                                   :height h
                                   :border "1px solid black"}}
    [:div.window__header
-    [:div.window__title title]]])
+    [:div.window__title title]]
+   [:div.windows__body
+    [content]]])
 
+(defn pick-frame []
+  [:form
+   [:div.row.optimist__form__row
+    [:div.col-4
+     [:label.optimist__form__label.input--w12
+      {:for "input-select"} "Problem Type"]]
+    [:div.col-8
+     [:label.optimist__form__label.input--w12
+      {:for "input-select"} "chosen Type"]]]
+   [:dib.row.optimist__form__row
+    [:div.col-4
+     [:label.optimist__form__label.input--w12
+      {:for "input-select"} "Model Type"]]
+    [:div.col-8
+     [:label.optimist__form__label.input--w12
+      {:for "input-select"} "chosen Type"]]]
+
+   [:div.optimist__form__row.optimist__form__actions
+    [:div.col-4
+     [:a.optimist__button.optimist__button--primary
+      {:href ""
+       :on-click (fn [e]
+                   (.preventDefault e)
+                   #_(re-frame/dispatch [::send-to-server]))}
+      "Ok"]]]])
+
+(defn display-frame []
+  (pick-frame))
 
 ;; FRAMES ==========================================
 
-(def frames [{:id 0, :title "Pick Data", :x 50, :y 100, :w 200, :h 200}
-             {:id 1, :title "Display Data", :x 50, :y 400, :w 200, :h 200}])
+(def frames [{:id 0, :title "Pick Data", :content pick-frame
+              :x 50, :y 100, :w 300, :h 230}
+             {:id 1, :title "Display Data", :content display-frame
+              :x 50, :y 400, :w 300, :h 230}])
 
 ;; SPA VIEW ========================================
 
