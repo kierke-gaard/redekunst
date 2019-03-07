2da00a6f550f1c0b70d653a8c8697e2335e4ecac
kierke-gaard <clausen.janhendrik@gmail.com>
2019-03-07 13:01:34 +0100

    cleanup

--- test/cljs/fullstack/core_test.cljs
+++
@@ -1,7 +0,0 @@
-(ns fullstack.core-test
-  (:require [cljs.test :refer-macros [deftest testing is]]
-            [fullstack.core :as core]))
-
-(deftest fake-test
-  (testing "fake description"
-    (is (= 1 2))))
--- test/cljs/fullstack/runner.cljs
+++
@@ -1,5 +0,0 @@
-(ns fullstack.runner
-    (:require [doo.runner :refer-macros [doo-tests]]
-              [fullstack.core-test]))
-
-(doo-tests 'fullstack.core-test)
